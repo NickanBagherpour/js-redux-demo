4b1bef350fe2b1b64fcb92603f47d78ff4275ec9
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2023-09-04 16:11:16 +0330

    feat: basic redux

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('From index');
+const { createStore } = require("redux");
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const initialState = {
+  numbOfCakes: 10,
+};
+
+// (previousState, action) => newState
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numbOfCakes: state.numbOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Update state',store.getState()));
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
