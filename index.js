1628d79b58594e9322ae42704b1479158beccdef
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2023-09-04 16:17:22 +0330

    feat: restock cacke

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const { createStore } = require("redux");
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 function orderCake() {
   return {
     type: CAKE_ORDERED,
     quantity: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
   };
 }
 
@@ -22,18 +30,26 @@ const reducer = (state = initialState, action) => {
         ...state,
         numbOfCakes: state.numbOfCakes - 1,
       };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numbOfCakes: state.numbOfCakes + action.payload,
+      };
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
-console.log('Initial state', store.getState());
+console.log("Initial state", store.getState());
 
-const unsubscribe = store.subscribe(() => console.log('Update state',store.getState()));
+const unsubscribe = store.subscribe(() =>
+  console.log("Update state", store.getState())
+);
 
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 unsubscribe();
