1bf30cdd985953b656f880a48ee4a501b7c93a47
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2023-09-05 11:37:28 +0330

    feat: add logger middleware to redux store

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
 
 const createStore = redux.createStore;
 const bindActionCreators = redux.bindActionCreators;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+
+const logger = reduxLogger.createLogger();
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -119,13 +123,10 @@ const rootReducer = combineReducers({
 });
 
 // const store = createStore(reducer);
-const store = createStore(rootReducer);
-
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial state", store.getState());
 
-const unsubscribe = store.subscribe(() =>
-  console.log("Update state", store.getState())
-);
+const unsubscribe = store.subscribe(() =>{});
 
 // store.dispatch(orderCake());
 // store.dispatch(orderCake());
